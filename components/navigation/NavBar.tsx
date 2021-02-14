9c2ce29c28ecbf39a90da9a8c991490b1f73d7ba
auratiger <auratiger00@gmail.com>
2021-02-14 12:40:15 +0200

    added logo to navigation bar

--- components/navigation/NavBar.tsx
+++ components/navigation/NavBar.tsx
@@ -1,13 +1,21 @@
 import react from 'react';
 import styles from '../../styles/navigation.module.css';
+import Image from 'next/image'
 
 const NavBar = () => {
 
     return (
         <div className={styles.header}>
-            
+            <Image 
+                src={"/logo.png"}
+                alt={"logo"}
+                className={styles.logo}
+                width={200}
+                height={70}
+            />
         </div>
     )
 }
+
 
 export default NavBar;
